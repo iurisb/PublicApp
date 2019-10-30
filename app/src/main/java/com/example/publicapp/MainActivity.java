2f5b03a35b9426261d015369e44c7637d0852c6c
Barreto <iurisbdeveloper@gmail.com>
2019-10-30 01:01:55 -0300

    Desenvolvendo Tela de Cadastro

--- app/src/main/java/com/example/publicapp/MainActivity.java
+++ app/src/main/java/com/example/publicapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.publicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //teste 2
+
+        Button button = (Button) findViewById(R.id.btnEntrar);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewUser.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/publicapp/NewUser.java
@@ -0,0 +1,14 @@
+package com.example.publicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user);
+    }
+}
